e817651dcd7f62b4571dae86e2bd722754aca158
TaymaZS <tajmazxan99@bk.ru>
2023-05-10 12:57:01 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,89 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+      v-bind:value="title" 
+      @input="title = $event.target.value"
+      class="input" 
+      type="text" 
+      placeholder="Название"
+      >
+      <input 
+      v-bind:value="body" 
+      @input="body = $event.target.value"
+      class="input" 
+      type="text" 
+      placeholder="Описание"
+      >
+      <button class="btn" v-on:click="createPost">Создать пост</button>
+    </form> 
+  </div>
+<div class="post" v-for="post in posts">
+  <div> <strong>Название: </strong> {{ post.title }}</div>
+  <div> <strong>Описание: </strong> {{ post.body }}</div>
+</div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание 3'}
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+    },
+  } 
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  
+}
+.input {
+  width: 100%;
+  height: 50px;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10 15px;
+  color: teal;
+  border: 1px solid teal;
+  height: 50px;
 }
 </style>
